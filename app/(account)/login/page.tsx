64c3516a11d2de73b238ce5a7ee4b961d4564247
Morojenko <Fapah123@gmail.com>
2024-02-28 16:42:01 +0600

    made Recovery Page

--- app/(account)/login/page.tsx
+++ app/(account)/login/page.tsx
@@ -1,62 +1,62 @@
-import Link from 'next/link'
-import { Metadata } from 'next'
+import Link from "next/link";
+import { Metadata } from "next";
 export const metadata: Metadata = {
-	title: 'Sheen | Login',
-	description: 'Login page',
-}
+  title: "Sheen | Login",
+  description: "Login page",
+};
 
 export default function LoginPage() {
-	return (
-		<>
-			<article className="flex flex-col max-w-[23.563rem]">
-				<h2 className="heading-2">Welcome back</h2>
-				<p className="mt-[.5rem] lg:mt-[1rem] text-xl">
-					Enter your email and password to sign in to the website.
-				</p>
-				<p className="mt-[.7rem] lg:mt-[1.5rem] text-sm">
-					Not signed up yet?{' '}
-					<Link
-						className="accent-underline"
-						href="/registration"
-					>
-						Sign up
-					</Link>
-				</p>
-			</article>
-			<form
-				className="max-w-[26.875rem] w-full mt-[2.5rem] md:mt-[2.2rem]"
-				method="get"
-			>
-				<div className="flex flex-col gap-y-[2rem] lg:gap-y-[4rem]">
-					<input
-						className="input-primary"
-						placeholder="Email Address"
-						type="email"
-						required
-					/>
-					<input
-						className="input-primary"
-						placeholder="Your Password"
-						type="password"
-						required
-					/>
-				</div>
-				<label className="mt-[1rem] lg:mt-[1.7rem] flex items-center gap-x-[.625rem] text-sm">
-					<input type="checkbox" />
-					Keep me logged in
-				</label>
-				<input
-					className="w-full mt-[2rem] lg:mt-[4rem] button-primary"
-					type="submit"
-					value="Sign in"
-				/>
-				<Link
-					className="mt-[.7rem] lg:mt-[1.5rem] block w-max mx-auto text-sm accent-underline"
-					href="/recovery"
-				>
-					Forgot Password?
-				</Link>
-			</form>
-		</>
-	)
+  return (
+    <>
+      <article className="flex flex-col max-w-[23.563rem]">
+        <h2 className="heading-2">Welcome back</h2>
+        <p className="mt-[.5rem] lg:mt-[1rem] text-xl">
+          Enter your email and password to sign in to the website.
+        </p>
+        <p className="mt-[.7rem] lg:mt-[1.5rem] text-sm">
+          Not signed up yet?{" "}
+          <Link
+            className="accent-underline"
+            href="/registration"
+          >
+            Sign up
+          </Link>
+        </p>
+      </article>
+      <form
+        className="max-w-[26.875rem] w-full mt-[2.5rem] md:mt-[2.2rem]"
+        method="get"
+      >
+        <div className="flex flex-col gap-y-[2rem] lg:gap-y-[4rem]">
+          <input
+            className="input-primary"
+            placeholder="Email Address"
+            type="email"
+            required
+          />
+          <input
+            className="input-primary"
+            placeholder="Your Password"
+            type="password"
+            required
+          />
+        </div>
+        <label className="mt-[1rem] lg:mt-[1.7rem] flex items-center gap-x-[.625rem] text-sm">
+          <input type="checkbox" />
+          Keep me logged in
+        </label>
+        <input
+          className="w-full mt-[2rem] lg:mt-[4rem] button-primary"
+          type="submit"
+          value="Sign in"
+        />
+        <Link
+          className="mt-[.7rem] lg:mt-[1.5rem] block w-max mx-auto text-sm accent-underline"
+          href="/recovery"
+        >
+          Forgot Password?
+        </Link>
+      </form>
+    </>
+  );
 }
--- app/(account)/recovery/page.tsx
+++ app/(account)/recovery/page.tsx
@@ -1,9 +1,32 @@
-import { Metadata } from 'next'
+import { Metadata } from "next";
 export const metadata: Metadata = {
-	title: 'Sheen | Recovery',
-	description: 'Recovery page',
-}
+  title: "Sheen | Recovery",
+  description: "Recovery page",
+};
 
 export default function page() {
-	return <div>page</div>
+  return (
+    <>
+      <article className="flex flex-col max-w-[23.563rem]">
+        <h2 className="heading-2 capitalize">Lost Your Password?</h2>
+        <p className="mt-[.5rem] lg:mt-[1rem] text-base">
+          Lost your password? Please enter your email address. You will receive
+          a link to create a new password via email.
+        </p>
+      </article>
+      <form className="max-w-[26.875rem] w-full mt-[2.5rem] md:mt-[2.2rem]">
+        <input
+          className="input-primary w-full"
+          placeholder="Email Address"
+          type="mail"
+          required
+        />
+        <input
+          className="button-primary mt-[2rem] lg:mt-[4rem] w-full"
+          type="button"
+          value="Reset Password"
+        />
+      </form>
+    </>
+  );
 }

1f0477d3d19e375906637d50939aeee56e2f71d7
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2024-06-06 18:05:45 +0500

    item page

--- app/(dashboard)/shop/[id]/page.tsx
+++ app/(dashboard)/shop/[id]/page.tsx
@@ -2,6 +2,7 @@ import Item from "@/app/models/Item"
 import { Types } from "mongoose"
 import Image from "next/image"
 import { Suspense } from "react"
+
 export async function generateStaticParams() {
   const data: Item[] = await Item.find()
   return data.map((item: Item) => {
@@ -28,19 +29,60 @@ export default async function ItemPage({
   const data = (await Item.findById(params.id)) as Item
 
   return (
-    <section>
+    <section className="">
       <Suspense
         fallback={<h2 className="text-[4rem]/[4rem]">Item is loading...</h2>}
       >
-        <div>
-          <h2>{data.item}</h2>
+        <section>
           <Image
+            className="w-[50%]"
             src={data.img.src}
             width={data.img.width}
             height={data.img.height}
             alt={data.img.alt}
+            priority={true}
           />
-        </div>
+          {/* {Array.from({ length: 5 }).map((_, i) => {
+            return (
+              <Image
+                key={i}
+                src={data.img.src}
+                width={data.img.width}
+                height={data.img.height}
+                alt={data.img.alt}
+              />
+            )
+          })} */}
+        </section>
+        <article>
+          <h2>{data.item}</h2>
+          <p className="heading-2">{data.price}$</p>
+          <div>{/* Raiting */}</div>
+          <p>{data.aboutItem}</p>
+          <form action="submit">
+            <div>{/* size */}</div>
+            <input
+              className="input-primary"
+              defaultValue="1"
+            ></input>
+            <button className="button-primary">add to cart</button>
+          </form>
+          <p className="flex flex-row capitalize">
+            Categories:&ensp;
+            {data.categories.map((category, i) => {
+              return (
+                <span
+                  className="text-gray"
+                  key={i}
+                >
+                  {i + 1 !== data.categories.length
+                    ? `${category},${String.fromCharCode(160)}`
+                    : category}
+                </span>
+              )
+            })}
+          </p>
+        </article>
       </Suspense>
     </section>
   )
